0b86a214d80ddab4c87c375b643fc5b332940fb5
MohamedNasr <mohamednasr27122@gmail.com>
2026-04-09 14:52:28 +0200

    Q1

--- wwwwwwwwwwwwwwwwww/Program.cs
+++ wwwwwwwwwwwwwwwwww/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace wwwwwwwwwwwwwwwwww
+﻿using static wwwwwwwwwwwwwwwwww.Source;
+using static wwwwwwwwwwwwwwwwww.Product;
+using static wwwwwwwwwwwwwwwwww.Customer;
+using static wwwwwwwwwwwwwwwwww.Employee;
+using static wwwwwwwwwwwwwwwwww.Order;
+using static wwwwwwwwwwwwwwwwww.Student;
+
+namespace wwwwwwwwwwwwwwwwww
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3);
+
+
+            #endregion
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
